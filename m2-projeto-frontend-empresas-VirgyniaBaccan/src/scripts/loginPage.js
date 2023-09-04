b007c85de95909be085a46d8355588492dcb4f79
Virgynia Lopes Baccan <107268167+VirgyniaBaccan@users.noreply.github.com>
2023-09-04 18:45:25 -0300

    Add files via upload

---
+++ m2-projeto-frontend-empresas-VirgyniaBaccan/src/scripts/loginPage.js
@@ -0,0 +1,35 @@
+import { bodyLogin } from "./requests.js"
+
+
+authenticationUser()
+
+loginChangePages()
+
+bodyLogin()
+
+function authenticationUser() {
+    const token = localStorage.getItem("@kenz.emp:token")
+    const adm = JSON.parse(localStorage.getItem("isAdm"))
+
+    if (token) {
+        window.location.replace("./userPage.html");
+        if (adm) {
+            location.replace("./adminPage.html")
+        }
+    }
+}
+
+function loginChangePages() {
+    const buttonHome = document.querySelector(".button__home")
+    buttonHome.addEventListener('click', () => {
+        window.location.replace("/index.html")
+    })
+
+    
+    const buttonRegister = document.querySelectorAll("#button__register")
+    buttonRegister.forEach((button) => {
+    button.addEventListener('click', () => {
+        window.location.replace("./registerPage.html")
+    })
+})
+}
